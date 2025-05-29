dbf913ef182f421cc1d43b13c09eb7e3f2b800ee
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-05-29 16:07:04 +0400

    develop gtd

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,14 +1,16 @@
 import nodemailer from 'nodemailer';
 
+
 export const transporter = nodemailer.createTransport({
-    host: 'smtpdm-eu-central-1.aliyuncs.com', // SMTP host
+    host: 'smtp.eu.mailgun.org', // SMTP host
     port: 465, // Port for secure connection (usually 465 for SSL/TLS)
     secure:true,
     auth: {
-        user: 'service@mx.gtcmail.com', // SMTP username
-        pass: 'ServiceMail123', // SMTP password
+        user: 'portal@mx1.gtcmail.com', // SMTP username
+        pass: '48490f5c91f63ae6e470633d79aa10fb-7c5e3295-a24fb77e', // SMTP password
     },
 });
+
 
 export const mailOptions = {
     from: "service@mx.gtcmail.com",
