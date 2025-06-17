6a3400428f2ad835ab2c12ec881baca4921bb860
webteamgtc <144129216+webteamgtc@users.noreply.github.com>
2025-06-17 17:23:09 +0400

    updated card

--- app/[locale]/components/home/TierCards.jsx
+++ app/[locale]/components/home/TierCards.jsx
@@ -5,7 +5,7 @@ import { FaCrown, FaDiamond, FaGem } from "react-icons/fa6";
 const tiers = [
   {
     level: "Bronze",
-    range: "$0 – $1000",
+    range: "$0 – $100",
     icon: FaMedal,
     frontContent: "FX Research",
     backContent: ["FX Research"],
@@ -13,7 +13,7 @@ const tiers = [
   },
   {
     level: "Silver",
-    range: "$1000 – $5,000",
+    range: "$100 – $3,000",
     icon: FaAward,
     frontContent: "Bronze +\nCommodities Research\nRedeem Electronics",
     backContent: [
@@ -25,7 +25,7 @@ const tiers = [
   },
   {
     level: "Gold",
-    range: "$5,000 – $50,000",
+    range: "$3,000 – $10,000",
     icon: FaCrown,
     frontContent: "Silver +\nCrypto Research\nIndices Research",
     backContent: [
@@ -37,7 +37,7 @@ const tiers = [
   },
   {
     level: "Platinum",
-    range: "$50,000 – $100,000",
+    range: "$10,000 – $100,000",
     icon: FaDiamond,
     frontContent: "Gold +\nRedeem entertainment, watches, gold bars",
     backContent: [
